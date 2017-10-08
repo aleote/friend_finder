ea251fed99709990c7c8c84f5eac47719fe95aad
annalisa leote <amleote@gmail.com>
2017-10-08 17:54:57 -0400

    inital file setup

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,64 @@
+{
+  "name":"Todd weights",
+  "photo":"https://st2.depositphotos.com/1525321/9764/i/950/depositphotos_97644534-stock-photo-funny-weak-man-lifting-biceps.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Craig Runner",
+  "photo":"https://thumbs.dreamstime.com/b/running-man-runner-looking-camera-smiling-funny-flirtatious-playful-handsome-good-male-fitness-model-jogging-outside-38935197.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Janet Biking",
+  "photo":"https://previews.123rf.com/images/maridav/maridav1403/maridav140300314/26735738-Bike-helmet-woman-putting-biking-helmet-on-outside-during-bicycle-ride--Stock-Photo.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Macy Yoga",
+  "photo":"http://images.all-free-download.com/images/graphicthumb/stretching_and_exercising_205630.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
